0fda0de79079737cd40bc31bf3957b0b77a03f7a
tamimhasan13 <tamimhasan975@gmail.com>
2026-08-31 20:17:23 +0600

    cart remove added

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -59,3 +59,53 @@ export const updateCart=async(req,res)=>{
         
     }
 }
+
+// Remove product from cart
+export const removeFromCart = async (req, res) => {
+  try {
+    const { itemId, size } = req.body;
+    const userId = req.userId;
+
+    const userData = await userModel.findById(userId);
+
+    if (!userData) {
+      return res.json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    const cartData = userData.cartData || {};
+
+    // Product 
+    if (!cartData[itemId]) {
+      return res.json({
+        success: false,
+        message: "Product not found in cart",
+      });
+    }
+
+    // Size remove
+    delete cartData[itemId][size];
+
+    // Product size product remove
+    if (Object.keys(cartData[itemId]).length === 0) {
+      delete cartData[itemId];
+    }
+
+    await userModel.findByIdAndUpdate(userId, { cartData });
+
+    res.json({
+      success: true,
+      message: "Product removed from cart",
+      cartData,
+    });
+  } catch (error) {
+    console.log(error.message);
+
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -2,10 +2,11 @@
 import express from 'express';
 import authUser from '../middlewares/userAuth.js';
 import { addProduct } from '../controllers/productController.js';
-import { addToCart, updateCart } from '../controllers/cartController.js';
+import { addToCart, removeFromCart, updateCart } from '../controllers/cartController.js';
 
 const cartRouter=express.Router()
 cartRouter.post('/add',authUser,addToCart)
 cartRouter.post('/update',authUser,updateCart)
+cartRouter.post("/remove", authUser, removeFromCart);
 
 export default cartRouter;
